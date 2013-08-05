60688757ade6e7a6b87c156034c71d5a3920fa1c
Blake Grotewold <grotr@d-32-72.its.mnsu.edu>
2013-08-05 10:51:39 -0500

    Update stylesheet, add dummy files for planned blog features, catrize.

--- assets/js/grotr.js
+++ assets/js/grotr.js
@@ -1,16 +1,4 @@
 $().ready( function() {
-  $('.collapser').click(function() {
-    if($('#navbar').hasClass('collapsed'))
-    {
-      $('#navbar').removeClass('collapsed');
-      $('navitem').removeClass('collapsed');
-    }
-    else
-    {
-      $('#navbar').addClass('collapsed');
-      $('navitem').removeClass('collapsed');
-    }
-    
-  });
-
+  $('.navitem').tooltip();
 });
+
