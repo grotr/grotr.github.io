df014f3abd36f28b6bff995315c90c049483f928
Blake Grotewold <blake.grotewold@gmail.com>
2019-12-04 20:59:18 -0600

    Update groteworld.js

--- assets/groteworld.js
+++ assets/groteworld.js
@@ -2,5 +2,5 @@ function toggleMute() {
   const video = document.getElementById("video-frame");
   const mute = document.getElementById("mute-btn");
   video.muted = !video.muted;
-  mute.innerHTML = video.muted ? "&#x1f507;" : "&#x1f50a;"
+  mute.innerHTML = video.muted ? '<i class="fa fa-volume-mute"></i>' : '<i class="fa fa-volume-up"></i>';
 }
